a0d5fae342714b499d953a380eecb7d6425a8f68
18oliveira.charles@gmail.com <18oliveira.charles@gmail.com@47c64ad3-2beb-1bbc-340d-9a2657610c32>
2012-12-02 03:35:36 +0000

    teste

---
+++ trunk/code/doc/html/search/variables_6d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['movements',['movements',['../classSprite.html#a763bec8792903c07c36505928cc7d09f',1,'Sprite']]]
+];
---
+++ trunk/code/doc/html/search/variables_77.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['width',['width',['../classSprite.html#a0474e991c61a611fdd79d4414aee3e95',1,'Sprite']]]
+];
