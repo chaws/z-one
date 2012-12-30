22c84aa3eb9942004cf4b760eadb7be560691f8e
tiagogomespereira@gmail.com <tiagogomespereira@gmail.com@47c64ad3-2beb-1bbc-340d-9a2657610c32>
2012-12-30 18:27:24 +0000

    Montagem inicial da arquitetura do jogo.

--- trunk/code/src/game.cpp
+++ trunk/code/src/game.cpp
@@ -1,5 +1,29 @@
 #include "game.hpp"
 
+using namespace std;
+
+SDL_Surface* Game::load_image(string img){
+	//Inicializa a imagem a se carregada
+	SDL_Surface* loadedImage = NULL;
+	//Inicializa a imagem otimizada
+	SDL_Surface* optimizedImage =  NULL;
+	
+	//Carrega a imagem
+	loadedImage = IMG_Load(img.c_str());
+	
+	//Se a imagem foi carrega
+	if(loadedImage != NULL){
+		
+	}
+}
+
+void Game::game(){
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	screen = SDL_SetVideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,BPP,SDL_SWSURFACE);
+	
+}
+
 void Game::start(){
 	
 	
--- trunk/code/src/game.hpp
+++ trunk/code/src/game.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <string>
+
 #include "SDL/SDL.h"
 #include "SDL/SDL_ttf.h"
+#include "SDL/SDL_image.h"
 
 class Game{
+	
+	SDL_Surface* load_image(string img);
 	
 	static const int SCREEN_WIDTH = 800;	
 	static const int SCREEN_HEIGHT = 600;
@@ -12,5 +17,6 @@ class Game{
 	
 	public:	
 		game();
+		~game();
 		void start();
 }
