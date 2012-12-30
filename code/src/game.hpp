829481939a2b661b4ca2627606d18cba9f8e078e
tiagogomespereira <tiagogomespereira@gmail.com>
2012-12-30 17:55:29 +0000

    Montagem inicial da arquitetura do jogo.

--- code/src/game.hpp
+++ code/src/game.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_ttf.h"
+
+class Game{
+	
+	static const int SCREEN_WIDTH = 800;	
+	static const int SCREEN_HEIGHT = 600;
+	static const int BPP = 32;
+	const int FPS = 30;
+	
+	public:	
+		game();
+		void start();
+}
