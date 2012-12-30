e69f4ca7d90bc8e7d9d7102cdf2013444d89e3f3
tiagogomespereira@gmail.com <tiagogomespereira@gmail.com@47c64ad3-2beb-1bbc-340d-9a2657610c32>
2012-12-30 19:17:37 +0000

    Metodos load_image() e iniciou construtor do Game.

--- trunk/code/src/game.cpp
+++ trunk/code/src/game.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+void Game::start(){
+    
+	
+}
+
+void Game::update_time(){
+
+}
+
+void Game::get_input(){
+
+}
+
+void Game::colision_detect(){
+
+}
+
+void Game::render(){
+
+}
+
+void Game::apply_surface(int x, int y, SDL_Surface* src, SDL_Surface* dest) {
+    // Estrutura que irá conter as coordenadas indicado aonde será aplicada a superfície 'src' dentro da superície 'dest'
+    SDL_Rect offset;
+    
+    // Definindo as coordenadas
+    offset.x=x;
+    offset.y=y;
+    
+    // Dando o Blit das superfícies
+    SDL_BlitSurface( src, NULL, dest, &offset);
+}
+
 SDL_Surface* Game::load_image(string img){
-	//Inicializa a imagem a se carregada
+    //Inicializa a imagem a se carregada
 	SDL_Surface* loadedImage = NULL;
 	//Inicializa a imagem otimizada
 	SDL_Surface* optimizedImage =  NULL;
@@ -13,18 +46,25 @@ SDL_Surface* Game::load_image(string img){
 	
 	//Se a imagem foi carrega
 	if(loadedImage != NULL){
-		
+		//Cria a imagem otimizada
+        optimizedImage = SDL_DisplayFormat(loadedImage);
+        //Libera a imagem antiga da memória
+        SDL_FreeSurface(loadedImage);
 	}
+    
+    return optimizedImage;
 }
 
-void Game::game(){
+void Game::Game(){
+    // Inicializa todos os componentes do SDL
 	SDL_Init(SDL_INIT_EVERYTHING);
-	
+    // Define a tela do jogo 
 	screen = SDL_SetVideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,BPP,SDL_SWSURFACE);
-	
 }
 
-void Game::start(){
-	
-	
+void Game::~Game(){
+    // Libera a estrutura da tela do jogo
+    SDL_FreeSurface(screen);
+    // Libera todas as alocações do SDL (desliga)
+    SDL_Quit();
 }
