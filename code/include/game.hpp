4cfe57a66576606753bec03665cf320c8cdde7d3
18oliveira.charles <18oliveira.charles@gmail.com>
2013-01-10 09:01:49 +0000

    Formatei um pouco do código e tentei incluir um audio

--- code/include/game.hpp
+++ code/include/game.hpp
@@ -1,35 +1,99 @@
-#pragma once
+#ifndef GAME_HPP
+#define GAME_HPP
 
 #include <string>
-
-#include "SDL/SDL.h"
-#include "SDL/SDL_ttf.h"
-#include "SDL/SDL_image.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_image.h>
 
 using namespace std;
 
+/**
+ *@brief Classe principal do jogo. Cuida da entrada de inputs etc e talz
+ */
 class Game{
-    // Constantes
+    /**
+	 *
+	 */
     static const int SCREEN_WIDTH = 800;    
+    
+    /**
+	 *
+	 */
 	static const int SCREEN_HEIGHT = 600;
+	
+	/**
+	 *
+	 */
 	static const int BPP = 32;
+	
+	/**
+	 *
+	 */
 	static const int FPS = 30;
+	
+	/**
+	 *
+	 */
 	static const Uint32 VIDEO_OPTIONS = SDL_HWSURFACE | SDL_DOUBLEBUF;
     
-    // Atributos
+    /**
+	 *
+	 */
     SDL_Surface *screen;
     
-    //Métodos
+    /**
+	 *
+	 */
 	SDL_Surface* load_image(string img);
-    void apply_surface(int x, int y, SDL_Surface* src, SDL_Surface* dest);
+	
+	/**
+	 *
+	 */
+    void apply_surface(int x, int y, SDL_Surface * src, SDL_Surface * dest);
+    
+    /**
+	 *
+	 */
 	void update_time();
+	
+	/**
+	 *
+	 */
     void get_input();
+    
+    /**
+	 *
+	 */
     void colision_detect();
+    
+    /**
+	 *
+	 */
     void render();
 	
 	public:	
+	
+		/**
+		 *
+		 */
 		Game();
+		
+		/**
+		 *
+		 */
 		~Game();
+		
+		/**
+		 *
+		 */
 		void start();
+		
+		/**
+		 *
+		 */
 		void delay(int usec);
 };
+
+#endif
+
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-#include "game.hpp"
+#include <game.hpp>
+#include <sound.hpp>
 
 int main()
 {
-	Game *game = new Game();
+	Game * game;
+	Sound sound;
+
+	// Inicia o jogo e as configuracoes SDL
+	game = new Game;
+
+	// Toca um audio porqueira por enquanto
+	sound.playSound("media/audio/translate_tts.wav");
+
+	// Abre a tela
 	game->start();
+
+
+
 	game->delay(6000);
 	delete game;
 	
