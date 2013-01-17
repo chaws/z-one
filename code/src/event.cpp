5bcf80f1723133b0a05cc695cf11ff19ea23b648
18oliveira.charles <18oliveira.charles@gmail.com>
2013-01-17 00:49:24 +0000

    Alterações diversas

--- code/src/event.cpp
+++ code/src/event.cpp
@@ -18,15 +18,42 @@ using namespace std;
  */
 void Event::getWindowEvent()
 {
-
+	if(this->event.active.state == SDL_APPACTIVE)
+		this->addEvt(event.active.gain == 0 ? WINDOW_LOSTFOCUS : WINDOW_GAINEDFOCUS);
+	
+	if(this->event.active.state == SDL_APPINPUTFOCUS)
+		this->addEvt(event.active.gain == 0 ? WINDOW_LOSTFOCUS : WINDOW_GAINEDFOCUS);
 }
 
 /**
- *
+ *	Se essa função foi chamada é pq foi disparado um evento de mouse
  */
 void Event::getMouseEvent()
 {
-	
+	Evt * ev;
+	ev = (Evt *)malloc(sizeof(Evt));
+	if(this->event.type == SDL_MOUSEMOTION)
+	{
+		ev->x = this->event.motion.x;
+		ev->y = this->event.motion.y;
+		ev->dx = this->event.motion.xrel;
+		ev->dy = this->event.motion.yrel;
+		ev->type = MOUSE_MOVED;
+		addEvt(ev);
+		return;
+	}
+
+	ev->x = this->event.button.x;
+	ev->y = this->event.button.y;
+
+	if(this->event.type == SDL_MOUSEBUTTONDOWN)
+		ev->type = (event.button.button == SDL_BUTTON_LEFT ? MOUSE_LCLICKED : 
+			(event.button.button == SDL_BUTTON_MIDDLE ? MOUSE_MCLICKED : MOUSE_RCLICKED));
+	else
+		ev->type = (event.button.button == SDL_BUTTON_LEFT ? MOUSE_LRELEASED : 
+			(event.button.button == SDL_BUTTON_MIDDLE ? MOUSE_MRELEASED : MOUSE_RRELEASED));
+
+	addEvt(ev);
 }
 
 /**
@@ -34,13 +61,19 @@ void Event::getMouseEvent()
  */
 void Event::getKeyboardEvent()
 {
-	
+	Evt * ev;
+	ev = (Evt *)malloc(sizeof(Evt));
+
+	ev->type = this->event.type == SDL_KEYDOWN ? KEY_PRESSED : KEY_RELEASED;
+	ev->key = this->event.key.keysym.sym;
+
+	this->q->push(ev);
 }
 
 /**
  *	Adiciona um novo evt na fila
  */
-void Event::addEvt(EvtType e, int x, int y, int dx, int dy)
+void Event::addEvt(EvtType e, int x, int y, int dx, int dy, SDLKey k)
 {
 	Evt * ev;
 	ev = (Evt *)malloc(sizeof(Evt));
@@ -49,7 +82,17 @@ void Event::addEvt(EvtType e, int x, int y, int dx, int dy)
 	ev->dx = dx;
 	ev->dy = dy;
 	ev->type = e;
+	ev->key = k;
 	this->q->push(ev);
+}
+
+
+/**
+ *	Adiciona um novo evt na fila
+ */
+void Event::addEvt(Evt * e)
+{
+	this->q->push(e);
 }
 
 /**
