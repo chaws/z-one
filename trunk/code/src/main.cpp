345da031b3b2c02a5bd9506d05f2e75794d8acbc
18oliveira.charles@gmail.com <18oliveira.charles@gmail.com@47c64ad3-2beb-1bbc-340d-9a2657610c32>
2013-02-10 17:25:30 +0000

    Adicionando mapa e a função principal

---
+++ trunk/code/src/main.cpp
@@ -0,0 +1,18 @@
+#include <aliado.h>
+#include <armadilha.h>
+#include <ataque.h>
+#include <botao.h>
+#include <desenhavel.h>
+#include <escutavel.h>
+#include <inimigo.h>
+#include <jogo.h>
+#include <mapa.h>
+#include <movimentavel.h>
+#include <tempo.h>
+#include <torre.h>
+
+int main()
+{
+	printf("aeeeeeeeeeeeee");
+	return 0;
+}
---
+++ trunk/code/src/mapa.cpp
@@ -0,0 +1,16 @@
+#include <mapa.h>
+
+int Mapa::desenhar()
+{
+	return 0;
+}
+
+Mapa::Mapa()
+{
+
+}
+
+Mapa::~Mapa()
+{
+
+}
