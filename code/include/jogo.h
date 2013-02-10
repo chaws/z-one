0fe82c2d635560e57e99b322d2613208d7f9ee70
matheus.souza21 <matheus.souza21@gmail.com>
2013-02-10 16:27:11 +0000

    Início da maratona de programação :D

---
+++ code/include/jogo.h
@@ -0,0 +1,17 @@
+#ifndef _JOGO_H_
+#define _JOGO_H_
+
+typedef enum 
+{
+	ENTRADA_JOGO,
+	MENU_PRINCIPAL,
+	JOGANDO,
+	FIM_JOGO
+} estadoJogo;
+
+class Jogo
+{
+	//
+};
+
+#endif
---
+++ code/include/mapa.h
@@ -0,0 +1,11 @@
+#ifndef _MAPA_H_
+#define _MAPA_H_
+
+typedef enum
+{
+	CAMINHO,
+	UTILIZAVEL,
+	NAO_UTILIZAVEL
+} tile;
+
+#endif
