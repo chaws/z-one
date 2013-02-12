cd52d063fee8ac5c98c8266f471eec86bfd78652
18oliveira.charles <18oliveira.charles@gmail.com>
2013-02-12 15:14:17 +0000

    Torre detectando inimigo proximo a ela de acordo com o alcance.

--- other/tiago/Torre atirando/inimigo.h
+++ other/tiago/Torre atirando/inimigo.h
@@ -5,13 +5,15 @@
 
 class inimigo{
 	SDL_Surface* image; //imagem do inimigo
-	SDL_Rect box; //retangulo que representa o inimigo
+	
 
 	int xVel;
 	int yVel;
 
 	bool colisao(SDL_Rect* rect1, SDL_Rect* rect2);
 public:
+	SDL_Rect box; //retangulo que representa o inimigo
+	
 	inimigo(SDL_Surface* img, int x, int y, int w, int h, int xvel, int yvel); //Construtor
 	~inimigo(); //Destrutor
 	
--- other/tiago/Torre atirando/torre.cpp
+++ other/tiago/Torre atirando/torre.cpp
@@ -1,4 +1,7 @@
 #include "torre.h"
+#include <iostream>
+
+using namespace std;
 
 torre::torre(SDL_Surface* img, int x, int y, int w, int h){
 	image=img;
@@ -6,6 +9,13 @@ torre::torre(SDL_Surface* img, int x, int y, int w, int h){
 	box.y=y;
 	box.w=w;
 	box.h=h;
+
+	//Inicializando a area de percepcao da torre
+	alcance.x = x-w;
+	alcance.y = y-h;
+	alcance.w = w*3;
+	alcance.h = h*3;
+	cor = SDL_MapRGB(SDL_GetVideoSurface()->format,0x00,0xff,0x00);
 }
 
 torre::~torre(){
@@ -16,6 +26,28 @@ void torre::show(){
 	SDL_BlitSurface(image,NULL,SDL_GetVideoSurface(),&box);
 }
 
+void torre::showAlcance(){
+	//cout << "mostrou o alcance." << endl;
+	SDL_FillRect(SDL_GetVideoSurface(),&alcance,cor);
+	SDL_UpdateRect(SDL_GetVideoSurface(),alcance.x,alcance.y,alcance.w,alcance.h);
+}
+
+bool torre::isInimigoProximo(SDL_Rect* inimigo){
+	if(alcance.y >= inimigo->y + inimigo->h)
+		return 0;
+	if(alcance.x >= inimigo->x + inimigo->w)
+		return 0;
+	if(alcance.y + alcance.h <= inimigo->y)
+		return 0;
+	if(alcance.x + alcance.w <= inimigo->x)
+		return 0;
+	return 1;
+}
+
 // void torre::shot(){
 // 	bala tiro(int x, int y, int w, int h, int xvel, int yvel);
 // }
+
+void torre::setCor(Uint8 r, Uint8 g, Uint8 b){
+	cor = SDL_MapRGB(SDL_GetVideoSurface()->format,r,g,b);
+}
