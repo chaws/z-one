9abd0b11205048aa990871fa2868cb1eebe8d0dc
matheus.souza21 <matheus.souza21@gmail.com>
2013-03-08 00:49:25 +0000

    Animacao com sprites nos inimigos

--- code/src/inimigo.cpp
+++ code/src/inimigo.cpp
@@ -26,6 +26,16 @@ Inimigo::Inimigo(int tipo, Mapa * mapaAtual)
 	this->hp_vermelho.w = 40;
 	this->hp_vermelho.h = 5;
 	
+	this->direcaoEsquerda = true;
+	this->frame = 0.;
+	for(int i=0; i<10 ;i++){
+		clip[i].x = i*this->rect->w;
+		clip[i].y = 0;
+		clip[i].h = clip[i].w = this->rect->h;
+	}
+	
+	this->offset = 5;
+	
 	this->recemCriado = true;	
 	this->pixelsAndados = Mapa::TILE_WIDTH;
 	this->estaMorto = false;
@@ -38,6 +48,21 @@ Inimigo::Inimigo(int tipo, Mapa * mapaAtual)
 			this->pontosVida = this->pontosVidaTotal = 300;
 			this->pontosExperiencia = 50;
 			this->velocidade = 2;
+			
+			for(int i=0; i<10; i++)
+			{
+				this->clip[i].x = i*45;
+				this->clip[i].y = 0;
+				this->clip[i].h = this->clip[i].w = 45;
+			}
+			this->rect->w = 45;			
+			this->rect->h = 45;
+			this->rect->x -= 5;			
+			this->rect->y -= 5;
+			
+			this->hp_verde.w = 45;
+			this->hp_vermelho.w = 45;
+			
 			break;
 
 		case SAQUEADOR:
@@ -94,6 +119,7 @@ int Inimigo::setarProximaPosicao()
 	proximaTile[3] = (this->tileAtual.y * Mapa::TILES_POR_LINHA) + (this->tileAtual.x + (!this->recemCriado ? 1 : 0));	// direita
 	
 	caminhos[0] = caminhos[1] = caminhos[2] = caminhos[3] = false;
+	
 	chavesEscolhidas[0] = chavesEscolhidas[1] = chavesEscolhidas[2] = -1;
 	caminhosPossiveis = 0;
 	posicaoAnterior = (this->tileAnterior.y * Mapa::TILES_POR_LINHA) + (this->tileAnterior.x);
@@ -118,6 +144,16 @@ int Inimigo::setarProximaPosicao()
 	
 		this->deltaX = (novaTileX - this->tileAtual.x) * this->velocidade; 
 		this->deltaY = (novaTileY - this->tileAtual.y) * this->velocidade;
+		
+		if(this->deltaX < 0)  //andando para a esquerda
+		{
+			this->direcaoEsquerda = true;
+			this->offset = 0;
+		} else if (this->deltaX > 0)
+		{
+			this->direcaoEsquerda = false;
+			this->offset = 5;
+		}
 	
 		this->tileAnterior = this->tileAtual;
 		this->tileAtual.x = novaTileX;
@@ -143,7 +179,7 @@ int Inimigo::desenhar()
 {
 	SDL_FillRect(SDL_GetVideoSurface(),&this->hp_vermelho,SDL_MapRGB(SDL_GetVideoSurface()->format,0xff,0x00,0x00));
 	SDL_FillRect(SDL_GetVideoSurface(),&this->hp_verde,SDL_MapRGB(SDL_GetVideoSurface()->format,0x00,0xff,0x00));
-	SDL_BlitSurface(this->imagem, NULL, SDL_GetVideoSurface(), this->rect);
+	SDL_BlitSurface(this->imagem, &this->clip[(int) (this->frame + 0.5)+this->offset], SDL_GetVideoSurface(), this->rect);
 	return 0;
 }
 
@@ -154,12 +190,15 @@ int Inimigo::fazerLogica()
 		this->setarProximaPosicao();
 		this->pixelsAndados = 0;
 	}
+	
+	this->frame+=0.2f;
+	if(this->frame>=4)
+		this->frame=0.f;
 		
 	this->rect->y += this->deltaY;
 	this->rect->x += this->deltaX;
 	
 	this->hp_vermelho.x = hp_verde.x = this->rect->x;
-	
 	this->hp_vermelho.y = hp_verde.y = this->rect->y-5;
 	
 	this->pixelsAndados += this->velocidade;
